0a2d95ae73c9e1b42e0d8e3cf697d798270bc298
Mickey <972207518@qq.com>
2019-04-27 09:59:10 +0800

    sider关闭添加动画效果

--- src/header.vue
+++ src/header.vue
@@ -10,5 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.g-header {
+}
 </style>
--- src/sider.vue
+++ src/sider.vue
@@ -1,14 +1,40 @@
 <template>
-  <div class="g-sider">
-    <slot></slot>
-  </div>
+  <transition name="fade">
+    <div
+      class="g-sider"
+      v-if="hide"
+    >
+      <slot></slot>
+      <button @click="hide = false">关闭</button>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
-  name: "GuluSider"
+  name: "GuluSider",
+  data() {
+    return {
+      hide: true
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.g-sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: all .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  margin-left: -200px
+}
 </style>
