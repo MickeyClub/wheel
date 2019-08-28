33de96536fb67c9c71694ca32ba26bb33704756e
MickeyClub <972207518@qq.com>
2019-08-28 22:52:50 +0800

    创建popover 组件

---
+++ src/popover.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="g-popover">
+    <div @click="xxx">
+      <div class="content-wrapper" v-if="visible">
+        <slot name="content"></slot>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GuluPopover',
+  props: {
+    span: {
+      type: [Number, String]
+    }
+  },
+  data() {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    xxx() {
+      this.visible = !this.visible
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.g-popover {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+  .content-wrapper {
+    position: absolute;
+    bottom: 100%;
+    left: 0;
+    border: 1px solid red;
+  }
+}
+</style>
